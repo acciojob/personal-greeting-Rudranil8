5eabc015777e41d61ed7e360076920b39710850b
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-10-27 22:56:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
 
-import React from "react";
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   const [name, setName] = useState("");
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+    
+//     <p>Enter your name:</p>
+//       <input onChange={(e) => setName(e.target.value)}></input>
+//       <p>{name}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
   const [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
-    
-    <p>Enter your name:</p>
-      <input onChange={(e) => setName(e.target.value)}></input>
-      <p>{name}</p>
+        <p>Enter your name:</p>
+        <input onChange={(e) => setName(e.target.value)}></input>
+        <p>Hello, {name}!</p>
     </div>
   )
 }

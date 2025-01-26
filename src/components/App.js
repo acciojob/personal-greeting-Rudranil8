ad5f08e9c7570fce33ce670fb8ef50e34521f6cd
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2025-01-26 21:11:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -34,17 +34,42 @@
 
 // export default App
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+
+//   const [name, setName] = useState("");
+//   return (
+//     <div>
+//         <p id="prompt">Enter your name:</p>
+//         <input id="nameInput" onChange={(e) => setName(e.target.value)}></input>
+//         <p id="greeting">Hello, {name}!</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-
-  const [name, setName] = useState("");
+  const [input,setInput] = useState('');
+  const onInput = (e)=>{
+      const {value} = e.target
+    setInput(value)
+  }
+  const onClear = ()=>{
+    setInput('')
+  }
   return (
     <div>
-        <p id="prompt">Enter your name:</p>
-        <input id="nameInput" onChange={(e) => setName(e.target.value)}></input>
-        <p id="greeting">Hello, {name}!</p>
+        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+    <input value={input} onChange={onInput} />
+    {!!input.length&&<p>Hello {input}!</p>}
+    <button onClick={onClear}>Clear</button>
     </div>
   )
 }
